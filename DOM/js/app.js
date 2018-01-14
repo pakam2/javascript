c829ded68b057e7707ca41c3a3c1ecd179cdf6ab
pakam2 <pkami.87@gmail.com>
2018-01-14 09:44:11 +0100

    organising files, adding DOM

---
+++ DOM/js/app.js
@@ -0,0 +1,17 @@
+
+/**
+ * Created by Jacek on 2016-01-11.
+ */
+
+document.addEventListener("DOMContentLoaded", function(){
+    /*
+    Poniżej napisz kod rozwiązujący zadania
+     */
+     var fooArticle = document.getElementById('first');
+     console.log(fooArticle);
+     var header = fooArticle.querySelectorAll('h2');
+     console.log(header.length);
+
+
+
+});
