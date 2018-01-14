7d2f620f17345f15612f4f38f3ab7fa3c9c6ee06
pakam2 <pkami.87@gmail.com>
2018-01-14 11:48:51 +0100

    DOM

--- DOM/js/app.js
+++ DOM/js/app.js
@@ -1,17 +1,33 @@
-
 /**
  * Created by Jacek on 2016-01-11.
  */
 
 document.addEventListener("DOMContentLoaded", function(){
+    var homeElement = document.getElementById("home");
+    var childElements = document.querySelector(".oferts").children;
+    var banner = document.querySelector(".ban");
+    var blocks = document.querySelectorAll(".block");
+    var links = document.querySelector(".links").children;
+
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
-     var fooArticle = document.getElementById('first');
-     console.log(fooArticle);
-     var header = fooArticle.querySelectorAll('h2');
-     console.log(header.length);
+     var timeout = setTimeout( function(){
+     var homeElementId = homeElement.id;
+     var homeElementClass = home.className;
+     homeElement.id = homeElementClass;
+     homeElement.className = homeElementId;
+     }, 5000);
 
 
+     //console.log(homeElement, typeof homeElement);
+     //console.log(childElements, typeof childElements);
+     //console.log(banner, typeof banner);
+     //console.log(blocks, typeof blocks);
+     //console.log(links, typeof links);
+
+     for (var i = 0; i < childElements.length; i++){
+        console.log("Typ to: " + typeof childElements[i] + ", warotść to " + childElements[i].innerText);
+     }
 
 });
